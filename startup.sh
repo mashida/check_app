736f4e54ea291b324bc42f6454ab45977a1347a6
mashida <serditov.na@gmail.com>
2024-04-29 19:58:40 +0300

    added migrate and update database

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Run migrations
+python manage.py migrate
+
+#Update database from csv
+python manage.py update_database_from_csv
+
 # Add your cron job
 echo "0 3 * * * /app/manage.py update_database_from_csv >> /var/log/cron.log 2>&1" | crontab -
 
